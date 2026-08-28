3bb06641ca78b9ef7a7f5ecead640a9c5b1d3a36
Shreya <shreyadevadiga247@gmail.com>
2026-08-28 17:32:22 +0000

    Fix Flask startup for Jenkins test1

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ def home():
 @app.route("/health")
 def health():
     return "APPLIcation is healthy"
-if __name__ = = "__main__":
+if __name__ == "__main__":
   app.run(host="0.0.0.0", port=5000)
 
